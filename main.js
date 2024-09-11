fd370f6151a427506e61f51ab6dfead522e9e672
Shuto-work <shuto.work2828@gmail.com>
2024-09-11 13:04:04 +0900

    update css
    add animation

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ $(".p-header__hamburger").on("click", function () {
   $(".p-header__nav").toggleClass("is-active");
   $(".p-header__hamburger-line").toggleClass("cross");
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".p-about__img, .p-blog__img");
+
+  const showImage = () => {
+    images.forEach((img, index) => {
+      const rect = img.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom > 0) {
+        setTimeout(() => {
+          img.classList.add("show");
+        }, index * 100); 
+      }
+    });
+  };
+
+  window.addEventListener("scroll", showImage);
+  showImage();
+});
